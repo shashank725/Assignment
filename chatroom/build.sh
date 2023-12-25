20d0c9234c4393d05d49011189040283e98e9b65
shashank725 <shashank.nov725@gmail.com>
2023-12-26 00:06:07 +0530

    [ADD] render.yaml

--- chatroom/build.sh
+++ chatroom/build.sh
@@ -2,10 +2,12 @@
 
 # exit on error
 set -o errexit
+# cd chatroom
 
 python -m pip install --upgrade pip
 
 pip install -r requirements.txt
 
+python manage.py makemigrations
+python manage.py migrate --run-syncdb
 python manage.py collectstatic --no-input
-python manage.py migrate
